a5751d35838d68276b390349e5e5d0b24b8e9a3b
Richard Nagy <nagy.richard.antal@gmail.com>
2022-03-14 16:48:00 +0100

    Added renamed get_git_folders, added comments

--- urltools.py
+++ urltools.py
@@ -1,5 +1,6 @@
 from requests.compat import urlsplit
 
-def get_url_folders(url) -> tuple[list[str], str]:
+# Extracts and separates folders and repository name from a URL
+def get_git_folders(url: str) -> tuple[list[str], str]:
     path = urlsplit(url).path.split('/')[1:]
-    return (path[:-1], path[-1])
+    return ( path[:-1], path[-1] )
