655500404aa467233ad9ccf9389edd3baf3d119c
Richard Nagy <nagy.richard.antal@gmail.com>
2022-03-16 13:43:23 +0100

    Added error tests for not installed packages

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
 python --version
+if [ $? -neq 0 ]; then
+    echo "Error: Python is not installed."
+    exit 1
+fi
+
 pip --version
+if [ $? -neq 0 ]; then
+    echo "Error: Pip is not installed."
+    exit 1
+fi
+
 git --version
+if [ $? -neq 0 ]; then
+    echo "Error: Git is not installed."
+    exit 1
+fi
 
 pip install -r ./requirements.txt
